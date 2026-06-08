4d0fc182020e76701ea81c314eebfee86c60d822
fadibouteraa <fadi.bouteraa@gmail.com>
2026-06-08 06:04:28 +0100

    Fix: Add compose.desktop.currentOs to jvmMain for Skiko native libraries

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -8,6 +8,12 @@ kotlin {
     jvm()
     
     sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
+            }
+        }
         val commonMain by getting {
             dependencies {
                 implementation(project(":formk-core"))
